2cd7a30524d1cf4cbe0fcbea31415b9800554906
Patryk Janicki <kontakt.janicki@gmail.com>
2019-01-03 18:37:47 +0100

    Add at_position method to board class

--- lib/fifteen_puzzle_solver/board.rb
+++ lib/fifteen_puzzle_solver/board.rb
@@ -1,14 +1,14 @@
 class FifteenPuzzleSolver::Board
+  attr_reader :width, :height
+
   def initialize(blocks, width)
     @blocks = blocks
     @width = width
+    @height = @blocks.count / width
   end
 
-  # Calculate dimensions
-  def dimensions
-    {
-      width: @width,
-      height: @blocks.count / @width,
-    }
+  # Get element value at position
+  def at_position(row, column)
+    @blocks[@width * row + column]
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,11 +3,20 @@ require_relative "../lib/fifteen_puzzle_solver/board"
 
 RSpec.describe "Board" do
   before do
+    # 1 2 3
+    # 4 5 6
+    # 7 0 8
     @board = FifteenPuzzleSolver::Board.new([1, 2, 3, 4, 5, 6, 7, 0, 8], 3)
   end
 
   it "calculate board dimensions" do
-    expect(@board.dimensions[:width]).to eq(3)
-    expect(@board.dimensions[:height]).to eq(3)
+    expect(@board.width).to eq(3)
+    expect(@board.height).to eq(3)
+  end
+
+  it "get element at position" do
+    expect(@board.at_position(0, 0)).to eq(1)
+    expect(@board.at_position(1, 0)).to eq(4)
+    expect(@board.at_position(2, 1)).to eq(0)
   end
 end
