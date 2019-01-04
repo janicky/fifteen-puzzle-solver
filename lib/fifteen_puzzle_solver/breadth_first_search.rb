991f7464e0b1e924feba9bee2ce7b5467645ea2f
Patryk Janicki <kontakt.janicki@gmail.com>
2019-01-04 20:47:47 +0100

    Create breadth first search solve method

--- lib/fifteen_puzzle_solver/breadth_first_search.rb
+++ lib/fifteen_puzzle_solver/breadth_first_search.rb
@@ -5,7 +5,27 @@ class FifteenPuzzleSolver::BreadthFirstSearch
   end
 
   def solve
-    frontier = []
+    frontier = Queue.new
+    frontier.push(FifteenPuzzleSolver::Node.new(nil, @board))
     explored = []
+
+    i = 0
+    while !frontier.empty?
+      node = frontier.pop
+      system "clear"
+      puts "\n#{node.board.display}\n"
+      puts "#{i}: #{frontier.length} - #{explored.count}"
+
+      if node.board.valid?
+        puts "Solved!"
+        break
+      end
+
+      node.board.neighbors(node, @order).each do |neighbor|
+        frontier.push(neighbor) unless explored.include?(node.state)
+      end
+      explored << node.state
+      i += 1
+    end
   end
 end
--- spec/breadth_first_search_spec.rb
+++ spec/breadth_first_search_spec.rb
@@ -2,4 +2,15 @@ require "fifteen_puzzle_solver"
 require_relative "../lib/fifteen_puzzle_solver/breadth_first_search"
 
 RSpec.describe "BreadthFirstSearch" do
+  before do
+    # 1 2 3
+    # 4 5 6
+    # 7 0 8
+    @board = FifteenPuzzleSolver::Board.new([9, 8, 3, 2, 11, 10, 12, 7, 13, 14, 4, 6, 15, 5, 0, 1], 4)
+    @bfs = FifteenPuzzleSolver::BreadthFirstSearch.new(@board, "rdul")
+  end
+
+  it "solve puzzle" do
+    @bfs.solve
+  end
 end
