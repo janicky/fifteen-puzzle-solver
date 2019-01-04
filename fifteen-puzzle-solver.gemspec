e1f6b63ea5d753f7b5e247a53c125b080cd0a8db
Patryk Janicki <kontakt.janicki@gmail.com>
2019-01-04 13:31:56 +0100

    Remove interaction from gem

--- fifteen-puzzle-solver.gemspec
+++ fifteen-puzzle-solver.gemspec
@@ -4,13 +4,12 @@ Gem::Specification.new do |s|
   s.date        = "2019-01-03"
   s.summary     = "Fifteen Puzzle Solver"
   s.description = "Fifteen Puzzle Solver - BFS, DFS, A*"
-  s.authors     = ["Patryk Janicki"]
+  s.authors     = ["Patryk Janicki", "Konrad Jachimstal"]
   s.email       = "kontakt.janicki@gmail.com"
   s.files       =
     [
       "lib/fifteen_puzzle_solver.rb",
       "lib/fifteen_puzzle_solver/board.rb",
-      "lib/fifteen_puzzle_solver/interaction.rb",
     ]
   s.homepage    =
     "https://github.com/janicky/fifteen-puzzle-solver"
--- lib/fifteen_puzzle_solver.rb
+++ lib/fifteen_puzzle_solver.rb
@@ -2,27 +2,6 @@ class FifteenPuzzleSolver
   def initialize
     @board = Board.new([*0..8].shuffle, 3)
   end
-
-  def interaction
-    continue = true
-    while continue
-      system "clear"
-      puts @board.display
-      case Interaction.read_char
-      when "\e[A"
-        @board.move("up")
-      when "\e[B"
-        @board.move("down")
-      when "\e[C"
-        @board.move("right")
-      when "\e[D"
-        @board.move("left")
-      else
-        continue = false
-      end
-    end
-  end
 end
 
 require "fifteen_puzzle_solver/board"
-require "fifteen_puzzle_solver/interaction"
