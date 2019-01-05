b239c110915c57c1d8c9d35d5eb6cbb6e3055d0e
Patryk Janicki <kontakt.janicki@gmail.com>
2019-01-05 21:12:55 +0100

    Add a star search tests

--- lib/fifteen_puzzle_solver/a_star_search.rb
+++ lib/fifteen_puzzle_solver/a_star_search.rb
@@ -2,6 +2,34 @@ require_relative "algorithm.rb"
 
 class FifteenPuzzleSolver::AStarSearch < FifteenPuzzleSolver::Algorithm
   def perform
-    # todo
+    @frontier = [FifteenPuzzleSolver::Node.new(nil, @board, nil)]
+    @explored = []
+
+    start
+    while !@frontier.empty?
+      node = @frontier.pop
+      @depth = node.depth
+
+      if node.board.valid?
+        @solution = node.path
+        @status = "solved"
+        break
+      end
+
+      neighbors = node.board.neighbors(node, "udlr")
+      heuristic_sort(@acronym, neighbors)
+
+      neighbors.reverse_each do |neighbor|
+        @frontier << neighbor unless @explored.include?(node.state)
+      end
+      @explored << node.state
+    end
+    save
+  end
+
+  private
+
+  def heuristic_sort(heuristic, nodes)
+    nodes.sort! { |a, b| a.astar_function(heuristic) <=> b.astar_function(heuristic) }
   end
 end
--- spec/a_star_search_spec.rb
+++ spec/a_star_search_spec.rb
@@ -3,11 +3,32 @@ require_relative "../lib/fifteen_puzzle_solver/a_star_search"
 
 RSpec.describe "AStarSearch" do
   before do
-    # 1 2 3
-    # 4 0 6
+    # 2 0 3
+    # 1 4 6
     # 7 5 8
-    @board = FifteenPuzzleSolver::Board.new([1, 2, 3, 4, 0, 6, 7, 5, 8], 3)
+    @board = FifteenPuzzleSolver::Board.new([2, 0, 3, 1, 4, 6, 7, 5, 8], 3)
     @astr = FifteenPuzzleSolver::AStarSearch.new(@board, "manh")
     @astr.perform
+  end
+
+  it "correctly solve specified board" do
+    expect(@astr.status).to eq("solved")
+  end
+
+  it "returns correct solution path" do
+    expect(@astr.solution).to eq("ldrdr")
+  end
+
+  it "returns correct recursion depth" do
+    expect(@astr.depth).to eq(5)
+  end
+
+  it "returns correct elapsed time" do
+    expect(@astr.elapsed_time).not_to eq(0)
+  end
+
+  it "returns visited nodes and processed nodes" do
+    expect(@astr.visited_nodes).to be(5)
+    expect(@astr.processed_nodes).to be(10)
   end
 end
