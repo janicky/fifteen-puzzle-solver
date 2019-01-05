a933c375521c4921a41b3d896cb19b9681d98c39
Patryk Janicki <kontakt.janicki@gmail.com>
2019-01-05 12:39:36 +0100

    Add direction to node parameter

--- lib/fifteen_puzzle_solver/node.rb
+++ lib/fifteen_puzzle_solver/node.rb
@@ -1,9 +1,10 @@
 class FifteenPuzzleSolver::Node
   attr_reader :board, :depth
 
-  def initialize(parent, board)
+  def initialize(parent, board, direction)
     @parent = parent
     @board = board
+    @direction = direction
     @depth = (parent ? parent.depth + 1 : 0)
   end
 
