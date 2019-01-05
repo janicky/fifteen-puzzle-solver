b2570da2cd4ea797e8ec657cb8b4d543e2efa630
Patryk Janicki <kontakt.janicki@gmail.com>
2019-01-05 17:25:47 +0100

    Depth first search perform method implementation

--- lib/fifteen_puzzle_solver/depth_first_search.rb
+++ lib/fifteen_puzzle_solver/depth_first_search.rb
@@ -2,6 +2,26 @@ require_relative "algorithm.rb"
 
 class FifteenPuzzleSolver::DepthFirstSearch < FifteenPuzzleSolver::Algorithm
   def perform
-    # todo
+    @frontier = [FifteenPuzzleSolver::Node.new(nil, @board, nil)]
+    @explored = []
+
+    start
+    while !@frontier.empty?
+      node = @frontier.pop
+      @depth = node.depth
+
+      if node.board.valid?
+        @solution = node.path
+        @status = "solved"
+      end
+
+      next if @depth > 20
+
+      node.board.neighbors(node, @order).reverse_each do |neighbor|
+        @frontier << neighbor unless @explored.include?
+      end
+      @explored << node.state
+    end
+    save
   end
 end
