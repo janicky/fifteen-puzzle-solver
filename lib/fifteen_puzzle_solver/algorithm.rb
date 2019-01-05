b32fee75477293677c5aef3feaab7919cf8d5152
Patryk Janicki <kontakt.janicki@gmail.com>
2019-01-05 11:59:11 +0100

    Add elapsed time to algorithm

--- lib/fifteen_puzzle_solver/algorithm.rb
+++ lib/fifteen_puzzle_solver/algorithm.rb
@@ -1,8 +1,26 @@
 class FifteenPuzzleSolver::Algorithm
-  attr_reader :status, :depth, :solution, :length
+  attr_reader :status, :depth, :solution, :length, :elapsed
 
   def initialize(board, order)
     @board = board
     @order = order
+    @status = "ready"
+  end
+
+  protected
+
+  def start
+    @start_time = clock_time
+  end
+
+  def save
+    @elapsed = clock_time - @start_time
+    @status = "failed" unless @status == "solved"
+  end
+
+  private
+
+  def clock_time
+    Process.clock_gettime(Process::CLOCK_MONOTONIC)
   end
 end
