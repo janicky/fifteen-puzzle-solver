aa6e4dc6192bbd471759d7d4c254406ad5aa9cc6
Patryk Janicki <kontakt.janicki@gmail.com>
2019-01-06 14:45:41 +0100

    Add basic validation to fifteen puzzle solver class

--- lib/fifteen_puzzle_solver.rb
+++ lib/fifteen_puzzle_solver.rb
@@ -1,6 +1,20 @@
 class FifteenPuzzleSolver
-  def initialize
-    @board = Board.new([*0..8].shuffle, 3)
+  def initialize(blocks, width, height, algorithm, acronym)
+    @blocks = blocks
+    @width = width
+    @height = height
+    @algorithm = algorithm
+    @acronym = acronym
+
+    validate
+  end
+
+  private
+
+  def validate
+    unless @block.count == @width * @height
+      raise Exception.new("Invalid blocks count (expect #{@width * @height}, got #{@blocks.count})")
+    end
   end
 end
 
